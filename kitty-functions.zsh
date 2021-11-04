2e328b41927b7d753d104def1a5928fd3ffe1c65
Joe Bloggs <vapniks@yahoo.com>
2021-11-04 14:50:29 +0000

    Add kitty-functions.zsh

---
+++ kitty-functions.zsh
@@ -0,0 +1,13 @@
+# Handy functions for use with the kitty terminal emulator
+function iplot {
+    cat <<EOF | gnuplot
+set terminal pngcairo enhanced font 'Fira Sans,10'
+set autoscale
+set samples 1000
+set output '|kitty +kitten icat --stdin yes'
+set object 1 rectangle from screen 0,0 to screen 1,1 fillcolor rgb"#fdf6e3" behind
+plot $@
+set output '/dev/null'
+EOF
+}
+
