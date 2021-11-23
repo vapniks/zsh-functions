2efd84661c9299fe6ece9c30de75d50c71b457e9
Joe Bloggs <vapniks@yahoo.com>
2021-11-23 17:45:19 +0000

    Add lsof-ipc-graph function

--- kitty-functions.zsh
+++ kitty-functions.zsh
@@ -11,3 +11,13 @@ set output '/dev/null'
 EOF
 }
 
+function lsof-ipc-graph () {
+    trap 'sudo pkill lsof && show-lsof-ipc-graph @' SIGINT
+    if [[ ! $@ == *@* ]]
+    then
+        eval "lsof -F ${@}" > /tmp/ipc-graph.lsof
+    fi
+    cat /tmp/ipc-graph.lsof | lsofgraph | unflatten -l 1 -c 6 | dot -Tjpg > /tmp/lsof-ipc-graph.jpg
+    kitty +kitten icat /tmp/lsof-ipc-graph.jpg
+}
+compdef _lsof lsof-ipc-graph
